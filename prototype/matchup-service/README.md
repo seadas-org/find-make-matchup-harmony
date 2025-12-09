17bcb6c97a9c712c96fb8a9f70e62e821580b7f6
aynur <aynur.abdurazik@nasa.gov>
2025-12-09 11:58:51 -0500

    Add prototype Harmony matchup service implementation

---
+++ prototype/matchup-service/README.md
@@ -0,0 +1,80 @@
+# Harmony Service Example
+
+A demonstration of a subsetter capability based on GDAL to be used with Harmony.
+
+These instructions only apply to building and developing the service.  The example
+service will be pulled automatically by Harmony from the `harmonyservices/service-example`
+repository on demand.
+## Prerequisites
+
+For building & pushing the image locally:
+
+1. [Docker](https://www.docker.com/get-started)
+
+For local development:
+
+1. miniconda3-24.7.1 (can be installed via pyenv)
+
+## Local Development
+
+### Install dependencies
+
+1. Create a [conda virtual environment](https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html):  `$ conda env create -n venv --file environment-dev.yml`
+2. Activate the virtual environment: `$ conda activate venv`
+
+### Run unit tests:
+
+        # Run the tests once:
+        $ make test
+
+        # Run the tests continuously in watch mode:
+        $ make test-watch
+
+### Developing with a local version of the Harmony Service Library
+
+You may want to test Harmony Service Example with an unreleased version of the Harmony Service Library.  This might be someone else's feature or bug-fix branch, or perhaps your own local changes. If you haven't already, clone the Harmony Service Library and switch to an unreleased branch or make your local changes. Typically this clone would be in a sibling directory of Harmony Service Example:
+
+        $ git clone https://github.com/nasa/harmony-service-lib-py ../harmony-service-lib-py
+
+Then install it into your conda environment in development mode. Subsequent changes to the Harmony Service Library will be reflected immediately without need to install it again:
+
+        $ pip install -e ../harmony-service-lib-py
+
+## Building & deploying the Docker image
+
+1. Build the Docker image (installs Harmony Service Library from PyPI):
+
+        $ make build-image
+
+If the Docker build does not complete and or this breaks your local Docker
+environment, try increasing the memory allocated to your Docker environment.
+
+If you'd like the Docker image to include a local version of the Harmony Service Library, set the `LOCAL_SVCLIB_DIR` environment variable to its location and build:
+
+        $ make build-image LOCAL_SVCLIB_DIR=../harmony-service-lib-py
+
+2. (Optional) Deploy (publish) the Docker image to Amazon ECR:
+
+        $ make push-image
+
+### Building from Dev Container
+
+If you plan to build the Docker image and are running Docker in Docker, you'll also want to create a .env file and populate it with the following:
+
+```
+# Harmony-Service-Example Environment Variables
+
+# Set to 'true' if running Docker in Docker and the docker daemon is somewhere other than the current context
+DIND=true
+
+# Set the PLATFORM varible to define the platform of the build output
+PLATFORM=linux/amd64
+
+# Indicates where docker commands should find the docker daemon
+DOCKER_DAEMON_ADDR=host.docker.internal:2375
+```
+
+## CI
+
+The project has a [Bamboo CI job](https://ci.earthdata.nasa.gov/browse/HARMONY-HG) running
+in the Earthdata Bamboo environment.
---
+++ prototype/matchup-service/harmony_service_example/matchup_adapter.py
@@ -0,0 +1,61 @@
+# harmony_service_example/matchup_adapter.py
+
+from typing import List
+from harmony import BaseHarmonyAdapter
+from harmony.util import download                # name may differ; check example
+from .matchup_engine import MatchupConfig, run_matchup
+
+class MatchupAdapter(BaseHarmonyAdapter):
+
+        def construct_config(self, job) -> MatchupConfig:
+                    # job should be Harmony job model; pull your custom params out of it
+                            params = job['params'] if isinstance(job, dict) else job.params
+                                    # choose names that match however you plan to call the service
+                                            return MatchupConfig(
+                                                                time_tolerance_sec=float(params.get('time_tolerance_sec', 1800)),
+                                                                            space_tolerance_km=float(params.get('space_tolerance_km', 5.0)),
+                                                                                        max_secondary_per_primary=int(params.get('max_secondary_per_primary', 1)),
+                                                                                                    primary_variables=params.get('primary_variables', []),
+                                                                                                                secondary_variables=params.get('secondary_variables', []),
+                                                                                                                        )
+
+                                                def split_primary_secondary(self, granule_paths: List[str]):
+                                                            # For a first cut, assume:
+                                                                    #   - first input granule is primary
+                                                                            #   - remaining are secondaries
+                                                                                    # Later you can use roles, collections, etc.
+                                                                                            primary = granule_paths[0]
+                                                                                                    secondary = granule_paths[1:]
+                                                                                                            return primary, secondary
+
+                                                                                                            def process_item(self, item, source):
+                                                                                                                        """
+                                                                                                                                Called by the base class for each input STAC item.
+                                                                                                                                        Should return a new STAC item describing the matchup output.
+                                                                                                                                                """
+                                                                                                                                                        job = self.message  # full Harmony job context
+                                                                                                                                                                cfg = self.construct_config(job)
+
+                                                                                                                                                                        # 1. Download input data to local disk
+                                                                                                                                                                                #    The example service shows the recommended way; this is conceptual:
+                                                                                                                                                                                        granule_paths = self.download(item)   # check exact name in example
+
+                                                                                                                                                                                                primary_path, secondary_paths = self.split_primary_secondary(granule_paths)
+
+                                                                                                                                                                                                        # 2. Decide where to write output
+                                                                                                                                                                                                                out_dir = self.staging_location  # from BaseHarmonyAdapter
+                                                                                                                                                                                                                        out_path = out_dir / "matchups.csv"
+
+                                                                                                                                                                                                                                # 3. Run the core algorithm
+                                                                                                                                                                                                                                        run_matchup(primary_path, secondary_paths, cfg, str(out_path))
+
+                                                                                                                                                                                                                                                # 4. Create a STAC item for the output and return it
+                                                                                                                                                                                                                                                        #    There are helpers in the service-lib to do this;
+                                                                                                                                                                                                                                                                #    mirror the pattern from the example subsetter.
+                                                                                                                                                                                                                                                                        output_item = self.create_stac_item_for_file(
+                                                                                                                                                                                                                                                                                            item=item,
+                                                                                                                                                                                                                                                                                                        path=str(out_path),
+                                                                                                                                                                                                                                                                                                                    mime_type="text/csv"
+                                                                                                                                                                                                                                                                                                                            )
+                                                                                                                                                                                                                                                                                return output_item
+
